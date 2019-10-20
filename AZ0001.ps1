79fee470dd316320ed1420c7dfb6fbc95cdd8c44
Joseph Choi <chousemath@gmail.com>
2019-10-20 15:53:38 +0900

    Store credentials in a variable and use in loop

--- AZ0001.ps1
+++ AZ0001.ps1
@@ -1,4 +1,20 @@
-﻿
+﻿<#
+# At startup, PowerShell loads only the core cmdlets by default. 
+# This means the cmdlets you need to work with Azure won't be loaded. 
+# The most reliable way to load the cmdlets you need is to import them manually at the start of your PowerShell session.
+Import-Module Az
+# You also need to authenticate before you can execute Az commands
+Connect-AzAccount
+#>
+
+<#
+# Determine which Az subscription is active
+Get-AzContext
+# Get a list of all your subscriptions
+Get-AzSubscription
+#>
+
+
 <#
 # List the names of all your resource groups
 Get-AzResourceGroup | ForEach-Object {$_.ResourceGroupName}
@@ -13,6 +29,8 @@ Remove-AzResourceGroup -Name AzTest
 <#
 # Create a new resource group
 New-AzResourceGroup -Name AzTest -Location 'Korea Central'
+# To create the resource groups, the Azure portal is a reasonable choice. 
+# These are one-off tasks, so you don't need scripts to do them.
 #>
 
 <#
@@ -48,12 +66,14 @@ Get-AzVM
 # Use a loop to create a coup VMs
 $resourceGroupName = "AzTest"
 $imageName = "UbuntuLTS"
+$c = Get-Credential
 For ($i = 0; $i -lt 3; $i++) {
     $vmName = "test-vm-$i"
     New-AzVM `
         -Name $vmName `
         -ResourceGroupName $resourceGroupName `
-        -Image $imageName
+        -Image $imageName `
+        -Credential $c
 }
 # AZ asks you for your credentials every time
 #>
