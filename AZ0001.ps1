5217aff8e6a7254b46bb0bb571bee1c3d431b45b
Joseph Choi <chousemath@gmail.com>
2019-10-22 08:48:52 +0900

    Practice moving resources with powershell

--- AZ0001.ps1
+++ AZ0001.ps1
@@ -77,3 +77,45 @@ For ($i = 0; $i -lt 3; $i++) {
 }
 # AZ asks you for your credentials every time
 #>
+
+<#
+# Moving a resource from one resource group to another
+# Store credentials for convenience
+$cred = Get-Credential
+
+# Create two new resource groups for this exercise
+New-AzResourceGroup -Name test0 -Location 'Korea Central'
+New-AzResourceGroup -Name test1 -Location 'Korea Central'
+
+# Create a VM (with associated resources) in the first resource group
+New-AzVM -ResourceGroupName test0 -Name vm0 -Image UbuntuLTS -Credential $cred
+
+# Gather all VM-related resources you created
+$vmResources = Get-AzResource -ResourceGroupName test0 -Name vm0
+$vmResources | ForEach-Object { Write-Host $_.ResourceGroupName }
+
+# Initiate the move from the first resource group to the other
+$vmResources | ForEach-Object { Move-AzResource -DestinationResourceGroupName test1 -ResourceId $_.ResourceId -Force }
+
+# Confirm that the resources were moved (output should be blank)
+$vmResources = Get-AzResource -ResourceGroupName test0 -Name vm0
+$vmResources | ForEach-Object { Write-Host $_.ResourceGroupName }
+
+# Confirm that the resources were moved (output should contain info on a couple resources)
+$vmResources = Get-AzResource -ResourceGroupName test1 -Name vm0
+$vmResources | ForEach-Object { Write-Host $_.ResourceGroupName }
+
+# Initiate the move back to the original resource group
+$vmResources | ForEach-Object { Move-AzResource -DestinationResourceGroupName test0 -ResourceId $_.ResourceId -Force }
+
+# Confirm that the resources were moved (output should be blank)
+$vmResources = Get-AzResource -ResourceGroupName test1 -Name vm0
+$vmResources | ForEach-Object { Write-Host $_.ResourceGroupName }
+
+# Confirm that the resources were moved (output should contain info on a couple resources)
+$vmResources = Get-AzResource -ResourceGroupName test0 -Name vm0
+$vmResources | ForEach-Object { Write-Host $_.ResourceGroupName }
+#>
+
+
+
