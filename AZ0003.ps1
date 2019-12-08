5bb70d28ee822c7075a9c33b3cc96460a6872d7c
Joseph Choi <chousemath@gmail.com>
2019-12-08 11:45:20 +0900

    Update with an new script

---
+++ AZ0003.ps1
@@ -0,0 +1,40 @@
+﻿$cred = Get-Credential
+
+$rg = New-AzResourceGroup rg01 -Location 'Korea Central'
+$vm = New-AzVM -ResourceGroupName rg01 -Name vm01 -Image UbuntuLTS -Credential $cred
+
+<# Run these commands in the Run Command section of the 
+ # Azure portal for your Ubuntu VM
+ # Shell commands run in the Azure console
+ $ sudo apt update
+ $ sudo apt install apache2 -y
+ $ sudo ufw app list
+ $ sudo ufw allow 'Apache'
+ $ sudo ufw status
+ $ sudo systemctl status apache2
+#>
+
+$nsg = Get-AzNetworkSecurityGroup -ResourceGroupName rg01 -Name vm01
+
+$nsg | Add-AzNetworkSecurityRuleConfig `
+    -Name OpenHttp `
+    -Description "Allow http traffic" `
+    -Access Allow `
+    -Protocol Tcp `
+    -Direction Inbound `
+    -Priority 100 `
+    -SourceAddressPrefix "Internet" `
+    -SourcePortRange * `
+    -DestinationPortRange 80 `
+    -DestinationAddressPrefix *
+
+$nsg | Set-AzNetworkSecurityGroup
+
+$ip = Get-AzPublicIpAddress -Name vm01
+$addr = $ip.IpAddress
+
+start microsoft-edge:"http://$addr"
+
+Remove-AzResourceGroup -Name rg01 -Force
+
+
